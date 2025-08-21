fa50fb49fd3d4dc4c64545583f6814a52f9b114b
daniel cano <krlag2011@gmail.com>
2025-08-21 09:47:29 -0500

    Proyecto inicial

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05627d1b7dc841fa22412c5fd5b82d28de116ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee2204df521cd0226919eca83953fa9f39937cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
